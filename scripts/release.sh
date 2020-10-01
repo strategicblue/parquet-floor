64c113c7957227c0956ac0ed9faee508209d3aa2
Tom Denley <t.denley@cantab.net>
2020-10-01 23:44:38 +0100

    tomd: release for real

---
+++ scripts/release.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -eu
+
+if [ -z ${JAVA_VERSION+x} ]; then
+  echo "JAVA_VERSION is unset, suggesting you are not running in CI, which is bad because this script will hurt your local machine" 1>&2
+  exit 1
+fi
+
+ROOT_DIR="$( cd "$( dirname "${0}" )/.." >/dev/null && pwd -P )"
+
+git -C "${ROOT_DIR}" log -n 1 --format=%an | grep -e "$GIT_AUTHOR_NAME" && exit 0
+
+sed -i 's/-SNAPSHOT//' "${ROOT_DIR}/pom.xml"
+
+"${ROOT_DIR}/scripts/run-mvn.sh" deploy release:update-versions -Dmaven.test.skip=true
+
+git -C "${ROOT_DIR}" add 'pom.xml'
+git -C "${ROOT_DIR}" commit -m 'released version [skip ci]'
