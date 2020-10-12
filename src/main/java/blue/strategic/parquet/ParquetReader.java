12b7ea0362caae02cd88415223a4facee3492142
Tom Denley <t.denley@strategic-blue.com>
2020-10-12 23:02:21 +0100

    tomd: refactor to remove need for static inner class.

--- src/main/java/blue/strategic/parquet/ParquetReader.java
+++ src/main/java/blue/strategic/parquet/ParquetReader.java
@@ -30,9 +30,18 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-public final class ParquetReader {
+public final class ParquetReader<U, S> implements Spliterator<S>, Closeable {
+    private final ParquetFileReader reader;
+    private final Hydrator<U, S> hydrator;
+    private final List<ColumnDescriptor> columns;
+    private final MessageType schema;
+    private final GroupConverter recordConverter;
+    private final String createdBy;
 
-    private ParquetReader() { /* prevent instantiation */ }
+    private boolean finished;
+    private long currentRowGroupSize = -1L;
+    private List<ColumnReader> currentRowGroupColumnReaders;
+    private long currentRowIndex = -1L;
 
     public static <U, S> Stream<S> readFile(File file, Hydrator<U, S> hydrator) throws IOException {
         InputFile f = new InputFile() {
@@ -64,10 +73,24 @@ public final class ParquetReader {
     }
 
     public static <U, S> Stream<S> readInputFile(InputFile file, Hydrator<U, S> hydrator) throws IOException {
-        ParquetFileReader reader = ParquetFileReader.open(file);
+        ParquetReader<U, S> pqReader = new ParquetReader<>(file, Collections.emptySet(), hydrator);
         return StreamSupport
-                .stream(new PqSpliterator<>(reader, Collections.emptySet(), hydrator), false)
-                .onClose(() -> closeSilently(reader));
+                .stream(pqReader, false)
+                .onClose(() -> closeSilently(pqReader));
+    }
+
+    private ParquetReader(InputFile file, Set<String> columnNames, Hydrator<U, S> hydrator) throws IOException {
+        this.reader = ParquetFileReader.open(file);
+        this.hydrator = hydrator;
+
+        FileMetaData meta = reader.getFooter().getFileMetaData();
+        this.schema = meta.getSchema();
+        this.recordConverter = new DummyRecordConverter(this.schema).getRootConverter();
+        this.createdBy = meta.getCreatedBy();
+
+        this.columns = schema.getColumns().stream()
+                .filter(c -> columnNames.isEmpty() || columnNames.contains(Arrays.deepToString(c.getPath())))
+                .collect(Collectors.toList());
     }
 
     private static void closeSilently(Closeable resource) {
@@ -75,88 +98,6 @@ public final class ParquetReader {
             resource.close();
         } catch (Exception e) {
             // ignore
-        }
-    }
-
-    private static class PqSpliterator<U, S> implements Spliterator<S> {
-        private final ParquetFileReader reader;
-        private final Hydrator<U, S> hydrator;
-        private final List<ColumnDescriptor> columns;
-        private final MessageType schema;
-        private final GroupConverter recordConverter;
-        private final String createdBy;
-
-        private boolean finished;
-        private long currentRowGroupSize = -1L;
-        private List<ColumnReader> currentRowGroupColumnReaders;
-        private long currentRowIndex = -1L;
-
-        PqSpliterator(ParquetFileReader reader, Set<String> columnNames, Hydrator<U, S> hydrator) {
-            this.reader = reader;
-            this.hydrator = hydrator;
-
-            FileMetaData meta = reader.getFooter().getFileMetaData();
-            this.schema = meta.getSchema();
-            this.recordConverter = new DummyRecordConverter(this.schema).getRootConverter();
-            this.createdBy = meta.getCreatedBy();
-
-            this.columns = schema.getColumns().stream()
-                    .filter(c -> columnNames.isEmpty() || columnNames.contains(Arrays.deepToString(c.getPath())))
-                    .collect(Collectors.toList());
-        }
-
-        @Override
-        public boolean tryAdvance(Consumer<? super S> action) {
-            try {
-                if (this.finished) {
-                    return false;
-                }
-
-                if (currentRowIndex == currentRowGroupSize) {
-                    PageReadStore rowGroup = reader.readNextRowGroup();
-                    if (rowGroup == null) {
-                        this.finished = true;
-                        return false;
-                    }
-
-                    ColumnReadStore columnReadStore = new ColumnReadStoreImpl(rowGroup, this.recordConverter, this.schema, this.createdBy);
-
-                    this.currentRowGroupSize = rowGroup.getRowCount();
-                    this.currentRowGroupColumnReaders = columns.stream().map(columnReadStore::getColumnReader).collect(Collectors.toList());
-                    this.currentRowIndex = 0L;
-                }
-
-                U start = hydrator.start();
-                for (ColumnReader columnReader: this.currentRowGroupColumnReaders) {
-                    hydrator.add(start, columnReader.getDescriptor().getPath()[0], readValue(columnReader));
-                    columnReader.consume();
-                    if (columnReader.getCurrentRepetitionLevel() != 0) {
-                        throw new IllegalStateException("Unexpected repetition");
-                    }
-                }
-
-                action.accept(hydrator.finish(start));
-                this.currentRowIndex++;
-
-                return true;
-            } catch (Exception e) {
-                throw new RuntimeException("Failed to read parquet", e);
-            }
-        }
-
-        @Override
-        public Spliterator<S> trySplit() {
-            return null;
-        }
-
-        @Override
-        public long estimateSize() {
-            return reader.getRecordCount();
-        }
-
-        @Override
-        public int characteristics() {
-            return ORDERED | NONNULL | DISTINCT;
         }
     }
 
@@ -168,25 +109,84 @@ public final class ParquetReader {
 
         if (columnReader.getCurrentDefinitionLevel() == maxDefinitionLevel) {
             switch (primitiveType.getPrimitiveTypeName()) {
-            case BINARY:
-            case FIXED_LEN_BYTE_ARRAY:
-            case INT96:
-                return stringifier.stringify(columnReader.getBinary());
-            case BOOLEAN:
-                return columnReader.getBoolean();
-            case DOUBLE:
-                return columnReader.getDouble();
-            case FLOAT:
-                return columnReader.getFloat();
-            case INT32:
-                return columnReader.getInteger();
-            case INT64:
-                return columnReader.getLong();
-            default:
-                throw new IllegalArgumentException("Unsupported type: " + primitiveType);
+                case BINARY:
+                case FIXED_LEN_BYTE_ARRAY:
+                case INT96:
+                    return stringifier.stringify(columnReader.getBinary());
+                case BOOLEAN:
+                    return columnReader.getBoolean();
+                case DOUBLE:
+                    return columnReader.getDouble();
+                case FLOAT:
+                    return columnReader.getFloat();
+                case INT32:
+                    return columnReader.getInteger();
+                case INT64:
+                    return columnReader.getLong();
+                default:
+                    throw new IllegalArgumentException("Unsupported type: " + primitiveType);
             }
         } else {
             return null;
         }
+    }
+
+    @Override
+    public void close() throws IOException {
+        reader.close();
+    }
+
+    @Override
+    public boolean tryAdvance(Consumer<? super S> action) {
+        try {
+            if (this.finished) {
+                return false;
+            }
+
+            if (currentRowIndex == currentRowGroupSize) {
+                PageReadStore rowGroup = reader.readNextRowGroup();
+                if (rowGroup == null) {
+                    this.finished = true;
+                    return false;
+                }
+
+                ColumnReadStore columnReadStore = new ColumnReadStoreImpl(rowGroup, this.recordConverter, this.schema, this.createdBy);
+
+                this.currentRowGroupSize = rowGroup.getRowCount();
+                this.currentRowGroupColumnReaders = columns.stream().map(columnReadStore::getColumnReader).collect(Collectors.toList());
+                this.currentRowIndex = 0L;
+            }
+
+            U start = hydrator.start();
+            for (ColumnReader columnReader: this.currentRowGroupColumnReaders) {
+                hydrator.add(start, columnReader.getDescriptor().getPath()[0], readValue(columnReader));
+                columnReader.consume();
+                if (columnReader.getCurrentRepetitionLevel() != 0) {
+                    throw new IllegalStateException("Unexpected repetition");
+                }
+            }
+
+            action.accept(hydrator.finish(start));
+            this.currentRowIndex++;
+
+            return true;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to read parquet", e);
+        }
+    }
+
+    @Override
+    public Spliterator<S> trySplit() {
+        return null;
+    }
+
+    @Override
+    public long estimateSize() {
+        return reader.getRecordCount();
+    }
+
+    @Override
+    public int characteristics() {
+        return ORDERED | NONNULL | DISTINCT;
     }
 }
